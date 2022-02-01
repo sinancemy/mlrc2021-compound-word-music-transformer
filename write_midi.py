ed6450f3327b9b1e2412bcfa674036b44151c512
Sierox <sinancem_yucel@hotmail.com>
2022-02-01 11:18:33 +0300

    first results

---
+++ write_midi.py
@@ -0,0 +1,84 @@
+# Taken (almost) verbatim from "https://github.com/YatingMusic/compound-word-transformer"
+
+import miditoolkit
+from miditoolkit.midi.containers import Marker, Instrument, TempoChange, Note
+import numpy as np
+import pickle
+import os
+
+BEAT_RESOL = 480
+BAR_RESOL = BEAT_RESOL * 4
+TICK_RESOL = BEAT_RESOL // 4
+
+def write_midi(words, path_outfile, word2event):
+    
+    class_keys = word2event.keys()
+    # words = np.load(path_infile)
+    midi_obj = miditoolkit.midi.parser.MidiFile()
+
+    bar_cnt = 0
+    cur_pos = 0
+
+    all_notes = []
+
+    cnt_error = 0
+    for i in range(len(words)):
+        vals = []
+        for kidx, key in enumerate(class_keys):
+            vals.append(word2event[key][words[i][kidx]])
+        # print(vals)
+
+        if vals[3] == 'Metrical':
+            if vals[2] == 'Bar':
+                bar_cnt += 1
+            elif 'Beat' in vals[2]:
+                beat_pos = int(vals[2].split('_')[1])
+                cur_pos = bar_cnt * BAR_RESOL + beat_pos * TICK_RESOL
+
+                # chord
+                if vals[1] != 'CONTI' and vals[1] != 0:
+                    midi_obj.markers.append(
+                        Marker(text=str(vals[1]), time=cur_pos))
+
+                if vals[0] != 'CONTI' and vals[0] != 0:
+                    tempo = int(vals[0].split('_')[-1])
+                    midi_obj.tempo_changes.append(
+                        TempoChange(tempo=tempo, time=cur_pos))
+            else:
+                pass
+        elif vals[3] == 'Note':
+
+            try:
+                pitch = vals[4].split('_')[-1]
+                duration = vals[5].split('_')[-1]
+                velocity = vals[6].split('_')[-1]
+                
+                if int(duration) == 0:
+                    duration = 60
+                end = cur_pos + int(duration)
+                
+                all_notes.append(
+                    Note(
+                        pitch=int(pitch), 
+                        start=cur_pos, 
+                        end=end, 
+                        velocity=int(velocity))
+                    )
+            except:
+                continue
+        else:
+            pass
+    
+    # save midi
+    piano_track = Instrument(0, is_drum=False, name='piano')
+    piano_track.notes = all_notes
+    midi_obj.instruments = [piano_track]
+    midi_obj.dump(path_outfile)
+
+
+
+path_dictionary =  os.path.join('../dataset/representations/uncond/cp/ailab17k_from-scratch_cp', 'dictionary.pkl')
+comp = np.load('out/cp/comp.npz')
+dictionary = pickle.load(open(path_dictionary, 'rb'))
+event2word, word2event = dictionary
+write_midi(comp, 'out/midi/comp.midi', word2event)
